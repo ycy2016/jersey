03be94ed50c0de1756b8c6810040e5ffb068cc47
ahycy2017 <ahycy2011@163.com>
2017-05-03 10:44:21 +0800

    演示

--- jerseyrestful/src/main/java/foo/Main.java
+++ jerseyrestful/src/main/java/foo/Main.java
@@ -35,7 +35,7 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-    	System.out.println("很厉害");
+    	System.out.println("很厉害!");
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
